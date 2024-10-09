9c1a4784d4a19c7a57eca0db158bd2b01a14d134
joaquin9830 <joaquin9811@gmail.com>
2024-10-09 18:23:14 -0300

    Configurando el servicio de Marvel

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,47 @@ export class MarvelService {
 
   constructor(private http: HttpClient) {}
 
+  private generateAuthParams() {
+    const ts = new Date().getTime();
+    const hash = CryptoJS.MD5(ts + this.privateKey + this.publicKey).toString();
+    return `ts=${ts}&apikey=${this.publicKey}&hash=${hash}`;
+  }
+
+  getCharacters(): Observable<any> {
+    const authParams = this.generateAuthParams();
+    return this.http.get(`${this.baseUrl}/characters?${authParams}`);
+  }
+
+  getCharacter(id: number): Observable<any> {
+    const authParams = this.generateAuthParams();
+    return this.http.get(`${this.baseUrl}/characters/${id}?${authParams}`);
+  }
+
+  // Métodos para manejar personajes en el localStorage
+  addLocalCharacter(character: any) {
+    let characters = this.getLocalCharacters();
+    characters.push(character);
+    localStorage.setItem('localCharacters', JSON.stringify(characters));
+  }
+
+  getLocalCharacters(): any[] {
+    const characters = localStorage.getItem('localCharacters');
+    return characters ? JSON.parse(characters) : [];
+  }
+
+  updateLocalCharacter(updatedCharacter: any) {
+    let characters = this.getLocalCharacters();
+    const index = characters.findIndex(c => c.id === updatedCharacter.id);
+    if (index > -1) {
+      characters[index] = updatedCharacter;
+      localStorage.setItem('localCharacters', JSON.stringify(characters));
+    }
+  }
+
+  deleteLocalCharacter(id: number) {
+    let characters = this.getLocalCharacters();
+    characters = characters.filter(c => c.id !== id);
+    localStorage.setItem('localCharacters', JSON.stringify(characters));
+  }
   
 }
