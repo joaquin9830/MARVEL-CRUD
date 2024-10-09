f99263628ced151f4e561c0bde10043f9b2cac44
joaquin9830 <joaquin9811@gmail.com>
2024-10-09 18:14:21 -0300

    Creando el componente header

--- src/app/services/marvel.service.ts
+++ src/app/services/marvel.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarvelService {
 
-  constructor() { }
+  private baseUrl = 'https://gateway.marvel.com/v1/public';
+  private publicKey = '5844c6ba3b9f8ddc0302a7f47ff0d779';
+  private privateKey = '92f81b16abffd686dd54461ea4dbd51ef8432a23';
+
+  constructor(private http: HttpClient) {}
+
+  
 }
