f87b096bcf03b696b67c7b19b5da80c75523ab1b
joaquin9830 <joaquin9811@gmail.com>
2024-10-10 10:13:13 -0300

    Estructura HTML | directivas | lógica ts de character-form

--- src/app/components/character-form/character-form.component.ts
+++ src/app/components/character-form/character-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-character-form',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './character-form.component.css'
 })
 export class CharacterFormComponent {
+  // El personaje que se editará será recibido desde el componente padre
+  @Input() character: any;
 
+  // Emitimos un evento cuando se guarda o cancela la edición
+  @Output() save = new EventEmitter<any>();
+  @Output() cancel = new EventEmitter<void>();
+
+  // Método para guardar el personaje editado
+  onSave() {
+    this.save.emit(this.character);
+  }
+
+  // Método para cancelar la edición
+  onCancel() {
+    this.cancel.emit();
+  }
 }
