2c7d583511e39e8528a276e590128511a89296b0
joaquin9830 <joaquin9811@gmail.com>
2024-10-10 10:11:24 -0300

    Configurando ts para el manejo de edición | eliminación

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-character-card',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './character-card.component.css'
 })
 export class CharacterCardComponent {
+  // El personaje será recibido desde el componente padre (ListComponent)
+  @Input() character: any;
 
+  // Eventos para editar y eliminar el personaje
+  @Output() edit = new EventEmitter<any>();
+  @Output() delete = new EventEmitter<number>();
+
+  // Método que emite el evento para editar
+  onEdit() {
+    this.edit.emit(this.character);
+  }
+
+  // Método que emite el evento para eliminar
+  onDelete() {
+    this.delete.emit(this.character.id);
+  }
 }
