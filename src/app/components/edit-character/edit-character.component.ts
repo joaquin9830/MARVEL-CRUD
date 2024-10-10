a812789e0bc0daaf2d7c4c1692fa0af1fe093cd7
joaquin9830 <joaquin9811@gmail.com>
2024-10-10 20:19:13 -0300

    Enlaces de header y nuevos enlaces

--- src/app/components/edit-character/edit-character.component.ts
+++ src/app/components/edit-character/edit-character.component.ts
@@ -6,14 +6,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './edit-character.component.css'
 })
 export class EditCharacterComponent {
+  //Binding de propiedades
   @Input() character: any;
+  //Binding de eventos
   @Output() update = new EventEmitter<any>();
   @Output() cancel = new EventEmitter<void>();
 
+  //Método para emitir el evento de actualización
   onSubmit() {
     this.update.emit(this.character);
   }
 
+  //Método para emitir el evento de cancelación
   cancelEdit() {
     this.cancel.emit();
   }
