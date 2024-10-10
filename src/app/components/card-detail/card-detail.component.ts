7aeabe5864e3ba04faca8bab414e891b47743b77
joaquin9830 <joaquin9811@gmail.com>
2024-10-10 10:34:09 -0300

    Lógica y estrucuta de card-detail

--- src/app/components/card-detail/card-detail.component.ts
+++ src/app/components/card-detail/card-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-card-detail',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './card-detail.component.css'
 })
 export class CardDetailComponent {
+  @Input() character: any;  // Recibir personaje seleccionado
+  @Output() close = new EventEmitter<void>();  // Evento para cerrar detalles
 
+  closeDetail(): void {
+    this.close.emit();  // Emitir evento para cerrar la vista de detalles
+  }
 }
